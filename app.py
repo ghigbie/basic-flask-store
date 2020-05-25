913be3f1c674b50b8ccad75323bf25ffe7b50074
Derfel Cadarn <gehigbie@gmail.com>
2020-05-25 16:40:54 -0700

    Add html path

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -18,6 +18,10 @@ stores = [
     }
 
 ]
+
+@app.route("/")
+def home():
+    return render_template('index.html')
 
 @app.route('/store', methods=['POST'])
 def create_store():
