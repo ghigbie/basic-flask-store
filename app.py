25ffe4b225f1b1d18a0e4b4e51466983627e6c61
Derfel Cadarn <gehigbie@gmail.com>
2020-05-25 14:44:48 -0700

    Update routes

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+#get all stores
+@app.route('/store')
+def get_stores():
+    pass
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
+
+
+app.run(port=5000)
